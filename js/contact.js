b980599b3a9038a549c3c120940fcbcd0dab92c7
B-mirza <bmirza@wisc.edu>
2018-01-04 19:47:52 +0400

    Completed Mobile Optimization

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,14 @@
-window.onload = function() {
-    document.getElementById("submitButton").onclick = submitForm;
-};
+(function () {
+    
+    window.onload = function() {
+        document.getElementById("submitButton").onclick = submitForm;
+    };
 
-function submitForm() {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
+    function submitForm() {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
 
-    alert("Your message has been sent to Helios");
-}
+        alert("Your message has been sent to Helios");
+    }
+})();
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,21 @@
-window.onload = function() {
-   document.getElementById("menuIcon").onclick = openNav;
-}
+(function () {
+   window.onload = function() {
+      document.getElementById("menuIcon").onclick = openNav;
+      document.getElementById("closebtn").onclick = closeNav;
+   }
 
-function openNav() {
-   document.getElementById("mobileMenu").style.width = "250px";
-}
+   function openNav() {
+      //Delay showing social media icons
+      setTimeout(function() {
+         document.getElementById("socialMediaIcons").style.display = "block";
+      }, 500);
+      document.getElementById("mobileMenu").style.width = "250px";
+      document.getElementById("container").style.backgroundColor = "rgba(0,0,0,0.2)";
+   }
 
-function closeNav() {
-   document.getElementById("mobileMenu").style.width = "0";
-}
+   function closeNav() {
+      document.getElementById("mobileMenu").style.width = "0";
+      document.getElementById("socialMediaIcons").style.display = "none";
+      document.getElementById("container").style.backgroundColor = "white";
+   }
+})();
